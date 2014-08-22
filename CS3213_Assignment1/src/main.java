fd6796a2a104617085e10e7a0ef8201954c99861
syncsophia <yuenshuhui@gmail.com>
2014-08-23 00:10:53 +0800

    testing on split

--- CS3213_Assignment1/src/main.java
+++ CS3213_Assignment1/src/main.java
@@ -1,9 +1,31 @@
+import java.util.*;
+
 
 public class main {
 
 	public static void main(String[] args) {
+		Map<String, String> myMap = new HashMap<String, String>();
+		
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		// do simple sorting
+		String input = "this is a simple testing";
+		ArrayList<String> temp = new ArrayList<String>();
+		String[] splitList = new String[10];
+		
+		splitList = input.split(" ");
+		
+		for(int i = 0; i < splitList.length; i++)
+		{
+			System.out.println(splitList[i]);
+			
+			temp.add(splitList[i]);
+		}
+		
+		temp.sort(null);
+		
+		System.out.println(temp.toString());
 
 	}
 
